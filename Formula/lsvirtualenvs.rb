75da03e8dda3bbfda63aab9ff58aeeb1b99284a2
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2021-05-11 18:54:17 +0300

    Fix typo in formula description

--- Formula/lsvirtualenvs.rb
+++ Formula/lsvirtualenvs.rb
@@ -1,5 +1,5 @@
 class Lsvirtualenvs < Formula
-  desc "Small commandline tool for `virtualenvwrapper`"
+  desc "Small command-line tool for `virtualenvwrapper`"
   homepage "https://github.com/vigo/lsvirtualenvs"
   url "https://github.com/vigo/lsvirtualenvs/archive/refs/tags/v0.1.0.tar.gz"
   sha256 "a59e1b428ba5c6f88b14f42d8621dd016b4c08de7d9e7b82b3b78debbd97e0e6"
