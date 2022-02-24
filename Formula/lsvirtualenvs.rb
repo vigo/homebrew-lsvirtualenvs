cc8d512fe498ce538b4f2950347792f0f4e4b389
Uğur Özyılmazel <ugurozyilmazel@gmail.com>
2022-02-24 20:23:09 +0000

    lsvirtualenvs: add 0.2.0 bottle.

--- Formula/lsvirtualenvs.rb
+++ Formula/lsvirtualenvs.rb
@@ -5,6 +5,12 @@ class Lsvirtualenvs < Formula
   sha256 "678654034a9369880c37b5be7ac7fc746413d5cbb5231f6e7ac7d85beb19cd7d"
   license "MIT"
 
+  bottle do
+    root_url "https://github.com/vigo/homebrew-lsvirtualenvs/releases/download/lsvirtualenvs-0.2.0"
+    sha256 cellar: :any_skip_relocation, big_sur:      "786f590351eec9e6322bfcf6b757628c25d7fb88f7d28a6b22418b0c719e02e8"
+    sha256 cellar: :any_skip_relocation, x86_64_linux: "3b13bacf6f645b19fac3ec483057b67b8678e40a21bcdecf943b09830ccbc39d"
+  end
+
   depends_on "go" => :build
 
   def install
